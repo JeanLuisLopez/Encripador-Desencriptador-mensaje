277de299636c4a02e2fa769d1316098a93245421
Jeanluislp <79077497+Jeanluislp@users.noreply.github.com>
2022-01-26 22:44:28 -0500

    final final

--- apuntes/jscopia.js
+++
@@ -1,8 +0,0 @@
-var botonencriptar = document.querySelector("#btn-encriptar");
-botonencriptar.addEventListener("click", function (event) {
-  event.preventDefault();
-
-  // var texto = document.getElementById("input-texto").value;
-  var accion = desencripatar();
-  document.getElementById("msg").value = accion;
-});
