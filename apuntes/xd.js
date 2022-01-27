e50733266e8e5540d12fd84680dbde2efe8f0bd9
Jeanluislp <79077497+Jeanluislp@users.noreply.github.com>
2022-01-26 22:42:27 -0500

    ultimosarreglos arreglos ultimos

--- apuntes/xd.js
+++ apuntes/xd.js
@@ -1,66 +1,29 @@
-var botonAdicionar = document.querySelector("#adicionar-paciente");
-botonAdicionar.addEventListener("click", function (event) {
-  event.preventDefault();
-  var form = document.querySelector("#form-adicionar");
+function encripatar(palabra) {
+  var texto = palabra;
+  var numchar = texto.length;
+  var salida = "";
+  var caracter;
 
-  var nombreform = form.nombre.value;
-  var pesoform = form.peso.value;
-  var alturaform = form.altura.value;
-  var gorduraform = form.gordura.value;
+  var i;
+  for (i = 0; i < numchar; i++) {
+    caracter = texto.charAt(i);
+    if (caracter === "a") {
+      caracter = "ai";
+    }
 
-  var pasienteTr = document.createElement("tr");
-  var nombreTd = document.createElement("td");
-  var pesoTd = document.createElement("td");
-  var alturaTd = document.createElement("td");
-  var gorduraTd = document.createElement("td");
-  var imcTd = document.createElement("td");
-  var tabla = document.querySelector("#tabla-pacientes");
-
-  nombreTd.textContent = nombreform;
-  pesoTd.textContent = pesoform;
-  alturaTd.textContent = alturaform;
-  gorduraTd.textContent = gorduraform;
-
-  pasienteTr.appendChild(nombreTd);
-  pasienteTr.appendChild(pesoTd);
-  pasienteTr.appendChild(alturaTd);
-  pasienteTr.appendChild(gorduraTd);
-  tabla.appendChild(pasienteTr);
-
-  console.log(pasienteTr);
-});
-var pasientes = document.querySelectorAll(".paciente");
-
-for (var i = 0; i < pasientes.length; i++) {
-  var pasiente = pasientes[i];
-  var tdPeso = pasiente.querySelector(".info-peso");
-
-  var peso = tdPeso.textContent;
-
-  var tdAltura = pasiente.querySelector(".info-altura");
-
-  var altura = tdAltura.textContent;
-
-  var tdIMC = pasiente.querySelector(".info-imc");
-
-  var pesoEsValido = true;
-  var alturaEsValida = true;
-
-  if (peso < 0 || peso > 100) {
-    tdIMC.textContent = "Peso erroneo";
-    pesoEsValido = false;
-    pasiente.classList.add("pasienteincorrecto");
+    if (caracter === "e") {
+      caracter = "enter";
+    }
+    if (caracter === "i") {
+      caracter = "imes";
+    }
+    if (caracter === "o") {
+      caracter = "ober";
+    }
+    if (caracter === "u") {
+      caracter = "ufat";
+    }
+    salida = salida + caracter;
   }
-
-  if (altura < 0 || altura > 3.0) {
-    tdIMC.textContent = "Altura erroneo";
-    alturaEsValida = false;
-    // pasiente.classList.add("pasienteincorrecto");
-  }
-
-  if (pesoEsValido && alturaEsValida) {
-    var imc = peso / (altura * altura);
-
-    tdIMC.textContent = imc.toFixed(2);
-  }
+  return salida;
 }
--- js/form.js
+++ js/form.js
@@ -18,3 +18,15 @@ botondesencriptar.addEventListener("click", function (event) {
   var resultado = desencriptar(texto);
   document.getElementById("msg").value = resultado;
 });
+
+function agrandarcuadrito(elemento) {
+  const textarea = document.querySelector(elemento);
+
+  textarea.addEventListener("keyup", (e) => {
+    let scHeight = e.target.scrollHeight;
+    textarea.style.height = `${scHeight}px`;
+  });
+}
+
+agrandarcuadrito("#input-texto");
+agrandarcuadrito("#msg");
